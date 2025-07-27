a8f1c29bdec9e88427b1dbdecb611fc34f2a8503
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2025-07-27 17:28:45 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 tailscaled -verbose 1 &
+tailscale up --auth-key=${ENV_TSAUTHKEY} --exit-node="${ENV_TSEXITNODE_IP}" --hostname=${ENV_TSHOSTNAME}
+
 ./RustDedicated -batchmode \
-        +server.identity "server3500.20250722" \
-        +server.hostname "nomarust Japan | JP | Vanilla | Monthly" \
+        +server.identity "server3500.20250722-2" \
+        +server.hostname "nomarust Test | JP | Vanilla | Monthly" \
         +server.description "Welcome to nomarust!\n---\ndiscord: https://discord.gg/nAyqFErqV4\n---\nこのサーバーは現在、運用テスト中です。\n詳細は Discord から確認してください。\n最大チーム人数:24 (同盟を許可しません)\nマップサイズ:3500\nワイプ予定日:2025/8/7 18:00 UTC\n---\nThis server is currently undergoing operational testing.\n Please check Discord for more details.\nTeam limit:24 (not allowed alliance)\nMap size:3500\nNext wipe schedule: 2025/8/7 18:00 UTC" \
         +server.logoimage "https://github.com/user-attachments/assets/9cb873a1-b0c8-4d01-9dfc-df41bb2468e5" \
         +server.url "https://discord.gg/Wr6yunTY" \
-        +server.seed "20250722" \
-        +server.worldsize 3500 \
+        +server.seed "${ENV_SEED:=$(date +%Y%m%d%H%M%S)}" \
+        +server.worldsize ${ENV_WORLDSIZE:=3000} \
         +server.maxplayers 100 \
         +server.maxconnectionsperip 500 \
         +app.maxconnectionsperip 500 \
         +relationshipmanager.maxteamsize 24 \
         +sv.secure 1 \
         +sv.EAC 1 \
-        +rcon.password "" \
-        +server.port 28015 \
-        +rcon.port 28016 \
-        +server.queryport 28017 \
-        +server.tags "PVP" \
-        +server.tags "JP" \
-        +server.tags "Monthly" &
+        +rcon.password "${ENV_RCON_PASSWD:=StrongPasswd123456}" \
+        +server.port ${ENV_SERVER_PORT:=28015} \
+        +rcon.port ${ENV_RCON_PORT:=28016} \
+        +server.queryport ${ENV_QUERY_PORT:=28017} \
+        +server.tags "PVP,Vanilla" &
 
 # 5分後に死活監視を開始
 for ((i = 1; i <= 300; i++))
