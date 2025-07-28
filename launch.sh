fd953067a4dbb8a9df70a21ce2c7949425b394f9
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2025-07-28 18:33:51 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -75,7 +75,7 @@ fi
         +server.hostname "${ENV_SERVERNAME:=TEST SERVER}" \
         +server.description "${ENV_SERVERDESCRIPTION:=Welcome!\n---\n---\nこのサーバーは現在テスト中です。}\nワイプ予定日:$(date -d "@$(cat ./wipeunixtime)" "+%Y/%m/%d %T")" \
         +server.logoimage "${ENV_SERVERLOGOIMG:=https://github.com/user-attachments/assets/9cb873a1-b0c8-4d01-9dfc-df41bb2468e5}" \
-        +server.url "${ENV_SERVERURL:=https://github.com/sakkuntyo/docker-rust-server" \
+        +server.url "${ENV_SERVERURL:=https://github.com/sakkuntyo/docker-rust-server}" \
         +server.seed "${seed}" \
         +server.worldsize ${ENV_WORLDSIZE:=3000} \
         +server.maxplayers ${ENV_MAXPLAYERS:=100} \
