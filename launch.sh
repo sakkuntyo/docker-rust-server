ed2a631642a71cc0a620e04ba190ff22f3bbde2c
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2025-08-11 11:02:30 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -149,7 +149,7 @@ while true; do
           ln -sf "sv.files.${createdServerVersion}.db-wal" "sv.files.$(((${createdServerVersion} + $i))).db-wal";
         done
         echo "INFO: サーバーデータのシンボリックリンクを作成しました。"
-        touch ../createdServerVersion
+        echo "${createdServerVersion}" > ../createdServerVersion
       )
       echo "INFO: createdServerVersion -> $(cat ./server/createdServerVersion)"
     fi
